9cc26fa0d5e72a5e26021416afc5a2fe1c046601
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-06-18 16:25:44 +0200

    Set GKE service account as notifications topic publisher

--- infrastructure/terraform/pubsub.tf
+++ infrastructure/terraform/pubsub.tf
@@ -1,3 +1,9 @@
 resource "google_pubsub_topic" "notifications" {
   name = var.pubsub.notifications_topic_name
 }
+
+resource "google_pubsub_topic_iam_member" "gke_sa-notifications_publisher" {
+  topic  = google_pubsub_topic.notifications.name
+  role   = "roles/pubsub.publisher"
+  member = "serviceAccount:${module.gke.service_account}"
+}
